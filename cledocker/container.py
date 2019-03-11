c1f27d5dffb4585213f4faa49b4741e6a12c2f18
Karolis Kalantojus <karolis@versada.eu>
2019-03-11 09:31:56 +0200

    use a separate tmp dir instead of a single file

--- cledocker/container.py
+++ cledocker/container.py
@@ -1,15 +1,18 @@
 """Utilities to work with generating/building docker images."""
 
-import tempfile
 import dataclasses
 import subprocess
+import os
 
 from . import constants
+from . import utils
 
 
 DOCKERFILE = """
 FROM debian:stable
-RUN apt install {options.package}
+RUN apt-get update && apt-get -y -q install \
+        {options.package} \
+    && rm -rf /var/lib/apt/lists/*
 """
 
 
@@ -21,13 +24,16 @@ def get_new_container_image_name(options):
 # FIXME add typing
 def build(docker_options):
     """Create a Docker image from new scratch file."""
-    with tempfile.NamedTemporaryFile() as handle:
+    with utils.NamedTemporaryDir() as tmp_dir:
+        with open(os.path.join(tmp_dir, 'Dockerfile'), 'w+b') as handle:
+            content = DOCKERFILE.format(options=docker_options)
+            handle.write(content.encode(constants.FILE_ENCODING))
 
-        content = DOCKERFILE.format(options=docker_options)
-        handle.write(content.encode(constants.FILE_ENCODING))
 
         image_name = get_new_container_image_name(docker_options)
-        subprocess.call(['docker', 'build', '-t', image_name, handle.name])
+        subprocess.call(['docker', 'build', '-t', image_name, tmp_dir])
+
+    # FIXME ability to retry ?
     return image_name
 
 
--- cledocker/utils.py
+++ cledocker/utils.py
@@ -1,5 +1,18 @@
 """General purpose utilities not belonging anywhere."""
 
+import contextlib
+import tempfile
+import shutil
+
 
 def get_new_executable_name(install_name):
     return f'docker-{install_name}'
+
+
+@contextlib.contextmanager
+def NamedTemporaryDir():
+    tmp_dir = tempfile.mkdtemp()
+    try:
+        yield tmp_dir
+    finally:
+        shutil.rmtree(tmp_dir)
